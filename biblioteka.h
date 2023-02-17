608c7a3dfaaa5355bbf7d2c4fd4f9bd0fb19f964
Starfire Swag <starfirs@at-r5.msk.21-school.ru>
2023-02-17 19:30:49 +0300

    test

---
+++ biblioteka.h
@@ -0,0 +1,18 @@
+#ifndef BIBLIOTEKA_H
+#define BIBLIOTEKA_H
+
+#include <stdio.h>
+
+// переводит с десятичного в двоичную систему
+void print_bits_state(char aCh);
+
+#define BIT_8 0x80
+#define BIT_7 0x40
+#define BIT_6 0x20
+#define BIT_5 0x10
+#define BIT_4 0x08
+#define BIT_3 0x04
+#define BIT_2 0x01
+#define BIT_1 0x01
+
+#endif  // BIBLIOTEKA_H
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "biblioteka.h"
+
+int main(int argc, char **argv) {
+  unsigned char ch = 0x01;
+  // 000000001
+  print_bits_state(ch);
+
+  return 0;
+}
